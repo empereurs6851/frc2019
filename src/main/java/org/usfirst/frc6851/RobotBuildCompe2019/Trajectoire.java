f504e8bb73a336430611ab66d895bfe04c10dc4e
Nicolas Larose <larose.nicolas@gmail.com>
2019-02-16 08:22:30 -0500

    trajectoire: nouvelle méthode de calcul

--- src/main/java/org/usfirst/frc6851/RobotBuildCompe2019/Trajectoire.java
+++ src/main/java/org/usfirst/frc6851/RobotBuildCompe2019/Trajectoire.java
@@ -3,22 +3,83 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.*;
-
+import java.lang.*;
 import edu.wpi.first.wpilibj.Notifier;
 import com.ctre.phoenix.motion.*;
 public class Trajectoire{
     public double[][] points;
-    public Trajectoire(double posDepart, double posFin, double vitesse, double acceleration)
+    private enum Etats 
     {
-        double DureeStable;
-        double DureeAcceleration;
-        double DureeTotale;
-        double distance = posFin - posDepart;
-        DureeStable = (distance - vitesse) / vitesse;
-        DureeAcceleration = vitesse / acceleration;
-        DureeTotale = 2 * DureeAcceleration +  DureeStable;
-        int nbPoints = (int)DureeTotale * 100;
-        points = new double[nbPoints][3];
-        
+        ARRET,
+        ACCELERATION,
+        MAINTIEN_VITESSE
+    }
+
+    private TalonSRX mMoteur;
+    private double mBaseDeTemp_sec;
+    private double mIncrementVitesse;
+    private double mIncrementVitesseActif;
+    
+    private double mIncrementPositionActif;
+    private double mIncrementPosition;
+    private double mVitesseMax;
+    private Etats mEtatTrajectoire;
+    private double mPositionActuelle;
+    private double mDestination;
+    public Trajectoire(TalonSRX moteur, double positionInit,double vitesseMax, double acceleration, double baseDeTemp_sec)
+    {
+        mMoteur = moteur;
+        mBaseDeTemp_sec = baseDeTemp_sec;
+        mVitesseMax = vitesseMax;
+        mIncrementVitesse = acceleration *  baseDeTemp_sec;
+        mIncrementPosition = mIncrementVitesse *  baseDeTemp_sec;
+        mEtatTrajectoire = Etats.ARRET;
+        mPositionActuelle = positionInit;
+        mDestination = positionInit;
+        mIncrementVitesseActif = 0;
+        mIncrementPositionActif = 0;
+    }
+    
+    public void SetPosition(double position)
+    {
+        mDestination = position;
+    }
+
+    public void Process()
+    {
+        switch(mEtatTrajectoire)
+        {
+            case ARRET:
+                if (mDestination != mPositionActuelle)
+                {
+                    mIncrementVitesseActif = mIncrementVitesse;
+                    if (mDestination < mPositionActuelle)
+                    {
+                        mIncrementVitesseActif *= -1;
+                    }
+                    mEtatTrajectoire = Etats.ACCELERATION; 
+                }
+                break;
+            case ACCELERATION:
+                if (Math.abs(mIncrementPositionActif) >= mIncrementPosition)
+                {
+                    if (mIncrementPositionActif < 0)
+                    {
+                        mIncrementPositionActif = -1 * mIncrementPosition;
+                    } 
+                    else
+                    {
+                        mIncrementPositionActif = mIncrementPosition;
+                    }
+                    mIncrementVitesseActif = 0;
+                    mEtatTrajectoire = Etats.MAINTIEN_VITESSE;
+                }
+                break;
+            case MAINTIEN_VITESSE:
+                break;
+
+        }
+        mIncrementPositionActif += mIncrementVitesseActif;
+        mPositionActuelle += mIncrementPositionActif;
     }
 }
